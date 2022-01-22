25be1fbbf14d38b2e31a9bc4911bb36394b85f1e
idurar <lalami.sdn@gmail.com>
2022-01-22 23:10:25 +0100

    💯 apollo client done !

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,3 @@
+export default function ErrorMessage({ message }: { message: string }) {
+  return <div className="error">{message}</div>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '@/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  const apolloClient = useApollo(pageProps);
 
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
 export default MyApp;
