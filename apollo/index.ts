77bd289562ef74300ce1b11c495c0cf925b8499f
idurar <lalami.sdn@gmail.com>
2022-01-24 03:01:08 +0100

    update env NEXT_PUBLIC_VERCEL_URL

--- apollo/index.ts
+++ apollo/index.ts
@@ -9,7 +9,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject>;
 const apiURL =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000/api/graphql'
-    : `${process.env.VERCEL_URL}/api/graphql`;
+    : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
 
 function createApolloClient() {
   return new ApolloClient({
